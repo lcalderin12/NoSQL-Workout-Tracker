bb0e5f1728be5671a09d6304929b60ce2c304fda
lcalderin12 <luiscalderin12@gmail.com>
2020-12-22 05:18:02 -0500

    clean up server

--- server.js
+++ server.js
@@ -13,11 +13,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost/workout", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
-
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/workout',
   {
